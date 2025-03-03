95191b4020c4bf5071e6daeb993f938ba10e4288
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-03 11:15:43 +0530

    .

--- SpringSeason.java
+++ SpringSeason.java
@@ -2,14 +2,23 @@ import java.util.Scanner;
 
 public class SpringSeason {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter month and day: ");
-        int month = scanner.nextInt();
-        int day = scanner.nextInt();
-        if ((month == 3 && day >= 20) || (month == 4) || (month == 5) || (month == 6 && day <= 20)) {
+        Scanner sc = new Scanner(System.in);
+
+	// Take user input
+        System.out.print("Enter the month (1-12): ");
+        int month = sc.nextInt();
+        System.out.print("Enter the day (1-31): ");
+        int day = sc.nextInt();
+	
+	//Check the month and day to determine if it's Spring Season or not
+        if ((month == 3 && day >= 20 && day <= 31) ||
+            (month == 4 && day >= 1 && day <= 30) ||
+            (month == 5 && day >= 1 && day <= 31) ||
+            (month == 6 && day >= 1 && day <= 20)) {
             System.out.println("It's a Spring Season");
         } else {
             System.out.println("Not a Spring Season");
         }
     }
 }
+
